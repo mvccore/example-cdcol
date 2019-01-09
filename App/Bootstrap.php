9e68b61e5829a7d01dbbac1d79f914c14390dfe3
Tom Flídr <tomflidr@gmail.com>
2019-01-09 11:52:09 +0100

    new packaging with static files copying functionality

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -15,6 +15,15 @@ class Bootstrap
 			$app->SetDebugClass('MvcCore\Ext\Debugs\Tracy');
 		}
 
+		
+		/**
+		 * Uncomment this line before generate any assets into temporary directory, before application 
+		 * packing/building, only if you want to pack application without JS/CSS/fonts/images inside
+		 * result PHP package and you want to have all those files placed on hard drive.
+		 * You can use this variant in modes `PHP_PRESERVE_PACKAGE`, `PHP_PRESERVE_HDD` and `PHP_STRICT_HDD`.
+		 */
+		//\MvcCore\Ext\Views\Helpers\Assets::SetAssetUrlCompletion(FALSE);
+
 
 		// Initialize authentication service extension and set custom user class
 		\MvcCore\Ext\Auths\Basic::GetInstance()
